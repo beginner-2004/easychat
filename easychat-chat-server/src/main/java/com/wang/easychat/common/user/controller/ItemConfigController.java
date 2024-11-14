730a1d3c1ca8ab5e2e655d1953f99c8085b565a3
wzd2004 <979208320@qq.com>
2024-11-14 20:26:13 +0800

    用户背包、背包物品相关类编写

---
+++ easychat-chat-server/src/main/java/com/wang/easychat/common/user/controller/ItemConfigController.java
@@ -0,0 +1,20 @@
+package com.wang.easychat.common.user.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 功能物品配置表 前端控制器
+ * </p>
+ *
+ * @author wang
+ * @since 2024-11-14
+ */
+@Controller
+@RequestMapping("/item-config")
+public class ItemConfigController {
+
+}
---
+++ easychat-chat-server/src/main/java/com/wang/easychat/common/user/service/impl/ItemConfigServiceImpl.java
@@ -0,0 +1,20 @@
+package com.wang.easychat.common.user.service.impl;
+
+import com.wang.easychat.common.user.domain.entity.ItemConfig;
+import com.wang.easychat.common.user.mapper.ItemConfigMapper;
+import com.wang.easychat.common.user.service.IItemConfigService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 功能物品配置表 服务实现类
+ * </p>
+ *
+ * @author wang
+ * @since 2024-11-14
+ */
+@Service
+public class ItemConfigServiceImpl extends ServiceImpl<ItemConfigMapper, ItemConfig> implements IItemConfigService {
+
+}
