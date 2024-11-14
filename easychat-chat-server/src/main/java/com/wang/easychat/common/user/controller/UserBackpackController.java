ae937bd7134c4f806eed4a16800b89be893b05d0
wzd2004 <979208320@qq.com>
2024-11-14 20:21:32 +0800

    Reapply "用户背包、背包物品相关类编写，解决spring 2.6.x 和 knife4j 冲突问题"
    This reverts commit 0d9f22abd35b5a67dbfbc782b185c2fe82984a06.

---
+++ easychat-chat-server/src/main/java/com/wang/easychat/common/user/controller/UserBackpackController.java
@@ -0,0 +1,20 @@
+package com.wang.easychat.common.user.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 用户背包表 前端控制器
+ * </p>
+ *
+ * @author wang
+ * @since 2024-11-14
+ */
+@Controller
+@RequestMapping("/user-backpack")
+public class UserBackpackController {
+
+}
